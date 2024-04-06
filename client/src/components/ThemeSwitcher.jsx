ae03f4db37865223edbd22238e9f4668dcaf7289
RoReeceDev <roreecedev@gmail.com>
2024-04-05 21:32:36 -0400

    dark mode component completed mvp

--- client/src/components/ThemeSwitcher.jsx
+++ client/src/components/ThemeSwitcher.jsx
@@ -1,20 +1,38 @@
 import useDarkMode from "use-dark-mode";
 import { Button } from "@material-tailwind/react";
+import { MoonIconFilled, SunIconFilled, SunIconEmpty, MoonIconEmpty } from "./DarkModeSVGs";
+
 
 const ThemeSwitcher = () => {
   const darkMode = useDarkMode(false);
+
+  console.log(darkMode)
+
 
   return (
     <>
 
       <div>
 
-        <Button type="button" onClick={darkMode.disable}>
-          ☀
-        </Button>
-        <Button type="button" onClick={darkMode.enable}>
-          ☾
-        </Button>
+        {darkMode.value ?
+          <div className="flex w-max gap-4">
+            <Button variant="text" className="rounded-full" type="button" onClick={darkMode.disable}>
+              <SunIconFilled />
+            </Button>
+            <Button variant="text" className="rounded-full" type="button" onClick={darkMode.enable}>
+              <MoonIconFilled />
+            </Button>
+          </div>
+           :
+           <div className="flex w-max gap-4">
+           <Button  variant="text" className="rounded-full" type="button" onClick={darkMode.disable}>
+             <SunIconEmpty />
+           </Button>
+           <Button variant="text" className="rounded-full" color="black" type="button" onClick={darkMode.enable}>
+             <MoonIconEmpty />
+           </Button>
+         </div>
+        }
 
       </div>
     </>
