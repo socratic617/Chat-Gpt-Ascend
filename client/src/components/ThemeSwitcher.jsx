79cd53f5d8ef63e8e804c8ec2c67fa1167cdfa54
RoReeceDev <roreecedev@gmail.com>
2024-04-12 17:41:21 -0400

    update styling on btn, making buttons smaller and icons centered, outline completed

--- client/src/components/ThemeSwitcher.jsx
+++ client/src/components/ThemeSwitcher.jsx
@@ -12,22 +12,38 @@ const ThemeSwitcher = ({ darkMode }) => {
         <div className="flex w-max gap-4">
             <Button
               variant="text"
-              className="rounded-full border-solid border-2	border-slate-700"
+              className="rounded-full border-solid border-1	border-gray-700 hover:border-gray-500 w-12 h-12 flex justify-center items-center p-2"
               type="button"
               onClick={darkMode.disable}
             >
               <SunIconFilled />
             </Button>
-          <Button variant="text" className="rounded-full" type="button" onClick={darkMode.enable}>
+          <Button 
+          variant="text" 
+          className="rounded-full border-solid border-1	border-gray-700 hover:border-gray-500 w-12 h-12 flex justify-center items-center p-2" 
+          type="button" 
+          onClick={darkMode.enable}
+          >
             <MoonIconFilled />
           </Button>
         </div>
         :
         <div className="flex w-max gap-4">
-          <Button variant="text" className="rounded-full" type="button" onClick={darkMode.disable}>
+          <Button 
+          variant="text" 
+          className="rounded-full border-solid border-1	border-black hover:border-gray-500 w-12 h-12 flex justify-center items-center p-2"
+          type="button" 
+          onClick={darkMode.disable}
+          >
             <SunIconEmpty />
           </Button>
-          <Button variant="text" className="rounded-full" color="black" type="button" onClick={darkMode.enable}>
+          <Button 
+          variant="text" 
+          className="rounded-full border-solid border-1 border-black hover:border-gray-500 w-12 h-12 flex justify-center items-center p-2"
+          color="black" 
+          type="button" 
+          onClick={darkMode.enable}
+          >
             <MoonIconEmpty />
           </Button>
         </div>
