5cacd495ae5a604936eea529b936a1b5b96591d5
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-04-13 20:24:24 -0400

    creatinf snapshot from component

--- client/__tests__/UserCard.test.jsx
+++ client/__tests__/UserCard.test.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { render } from 'vitest';
+
 import UserCard from '../src/components/UserCard.jsx';
-import { test } from 'vitest'; // 
+import { test, expect } from 'vitest'; 
 import App from '../src/App.jsx';
 
 test('renders user card with name and image', () => {
-    window.HTMLElement.prototype.scrollIntoView = () => {};
-    render(<App />);
- render(<UserCard></UserCard>)
+    // window.HTMLElement.prototype.scrollIntoView = () => {};
+    const result = <UserCard/>
+     expect(result).toMatchSnapshot()
 
-    const user = {
-        name: 'RC Student',
-        imgUser: 'https://example.com/user.jpg'
-    };
-
-    const { getByText, getByAltText } = render(<UserCard name={user.name} imgUser={user.imgUser} />);
-
-    expect(getByText(`Welcome, ${user.name}!`)).toBeInTheDocument(); // Use toBeInTheDocument() for text elements
-    expect(getByAltText('RC Student')).toBeInTheDocument(); // Use toBeInTheDocument() for image elements
 });
