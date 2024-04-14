7893aa994976a9497e91011eb2b9936e4fc90ba8
Danny Garcia Cortes <dannygarciadev@gmail.com>
2024-04-14 13:10:18 -0400

    creating comments describing every line of code

--- client/__tests__/UserCard.test.jsx
+++ client/__tests__/UserCard.test.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 import UserCard from '../src/components/UserCard.jsx';
 import { test, expect } from 'vitest'; 
-import App from '../src/App.jsx';
+
+
 
 test('renders user card with name and image', () => {
-    // window.HTMLElement.prototype.scrollIntoView = () => {};
-    const result = <UserCard/>
-     expect(result).toMatchSnapshot()
 
+
+    const user = { //creating a user name with and img to be render on DOM when rending app
+        name: "Daniel Garcia",
+        img: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+    };
+
+    const result = <UserCard name={user.name} img={user.img} />; // making varible with component and props 
+
+
+    expect(result).toMatchSnapshot(); // we expect that component to match out snapshot be the same
 });
